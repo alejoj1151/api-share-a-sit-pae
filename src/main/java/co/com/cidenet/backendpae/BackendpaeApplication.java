58504a4eea4b5bba6c4fdcd8b10509f6b4111e37
Juan Pablo <pablo-238@hotmail.com>
2018-08-30 19:40:29 -0500

    Commit inicial

---
+++ src/main/java/co/com/cidenet/backendpae/BackendpaeApplication.java
@@ -0,0 +1,15 @@
+package co.com.cidenet.backendpae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class BackendpaeApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BackendpaeApplication.class, args);
+    }
+}
